24acb6ffa4003442e29c2ff86bc041eb8838f215
Tom Rein <tomjrein@gmail.com>
2018-11-16 14:57:19 -0500

    added initial change event for block

--- src/components/Block.js
+++ src/components/Block.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
+import {Form, Button} from 'semantic-ui-react';
 
 class Block extends Component {
   state = {
     id: 1,
     parent: 1,
-    data: 1,
-    hash: "hello"
+    data: "",
+    hash: null
   };
 
+  handleChange = e => {
+    const value = e.target.value;
+    const name = e.target.name;
+
+    this.setState({ [name]: value});
+  }
 
   render () {
     const {data, hash} = this.state;
     return (
       <div className="ui container">
         <h1>Block</h1>
+        <Form size="big">
+          <Form.TextArea
+            label = "Data"
+            placeholder = "Data"
+            name = "data"
+            value = {data}
+            onChange={this.handleChange}
+          />
+        </Form>
+
+
+        <p> {hash} </p>
       </div>
     );
   }
